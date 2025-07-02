0daac6b6f5d550ff5dc8c5c48aee740f9042cf98
Irving Jose Duran <ijd.irving@gmail.com>
2025-07-02 18:07:40 -0400

    stripe checkout link success

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
           quantity: 1,
         },
       ],
-      success_url: `${process.env.NEXT_PUBLIC_APP_URL}/dashboard?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.NEXT_PUBLIC_APP_URL}`,
       cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/pricing`,
       metadata: {
         userId,
